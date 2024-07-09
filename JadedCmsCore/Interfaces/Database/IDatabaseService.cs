2e6528d16e5224fe33b0f9858ebc5821cc144642
kaustav halder <kaustavhalder2@gmail.com>
2024-07-09 22:59:15 +0530

    Database Service in progress

--- JadedCmsCore/Interfaces/Database/IDatabaseService.cs
+++ JadedCmsCore/Interfaces/Database/IDatabaseService.cs
@@ -4,6 +4,11 @@ namespace JadedCmsCore.Interfaces.Database;
 
 public interface IDatabaseService
 {
+    string ConnectionString { get; set; }
+    IDbConnection Conection { get; set; }
+    void OpenConection();
+    void CloseConnection();
+    
     Task<IEnumerable<T>> ExecuteQueryAsync<T>(string query, IEnumerable<IDbDataParameter> parameters = null);
     Task<IEnumerable<T>> ExecuteStoredProcedureAsync<T>(string storedProcedureName, IEnumerable<IDbDataParameter> parameters = null);
     Task ExecuteCommandAsync(string command, IEnumerable<IDbDataParameter> parameters = null);
--- JadedCmsCore/Services/Database/MsSqlDbService.cs
+++ JadedCmsCore/Services/Database/MsSqlDbService.cs
@@ -6,10 +6,13 @@ namespace JadedCmsCore.Services.Database;
 
 public class MsSqlDbService : IDatabaseService
 {
-    private string _connectionString { get; }
+    private string ConnectionString { get; }
+    
+    private IDbConnection Conection { get; set; }
     public MsSqlDbService(IConfiguration configuration)
     {
-        _connectionString = configuration["ConnectionStrings:DbConnection"];
+        ConnectionString = configuration["ConnectionStrings:DbConnection"];
+        
     }
     
     public Task<IEnumerable<T>> ExecuteQueryAsync<T>(string query, IEnumerable<IDbDataParameter> parameters = null)
