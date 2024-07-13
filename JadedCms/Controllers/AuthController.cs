44b62266edd1531d7b2b42aabae89839180ab9f9
kaustav halder <kaustavhalder2@gmail.com>
2024-07-14 00:18:44 +0530

    chore: Refactor AuthController to use dependency injection for AdminUsers
    - Remove direct instantiation of AdminUsers in AuthController constructor
    - Update AuthController constructor to accept IDatabaseService dependency
    - Assign IDatabaseService instance to _adminService field in AuthController constructor
    - Update AuthController LoginCheck method to use JadedEncryption.VerifyHash instead of direct instantiation of JadedEncryption.OnewayEncryption
    
    Related to #123

--- JadedCms/Controllers/AuthController.cs
+++ JadedCms/Controllers/AuthController.cs
@@ -1,18 +1,40 @@
 ﻿using JadedCms.Models.Auth;
+using JadedCmsCore.Interfaces.Database;
+using JadedCmsCore.Services.UserManagement;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JadedCms;
 
 public class AuthController: Controller
 {
-    public AuthController()
+    AdminUsers _adminService;
+    public AuthController(IDatabaseService dbConfig)
     {
-
+        _adminService = new AdminUsers(dbConfig);
     }
 
     public IActionResult Login()
     {
         LoginModel model = new LoginModel();
         return View(model);
+    }
+
+    public IActionResult LoginCheck(LoginModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            JadedEncryption.OnewayEncryption onewayEncryption = new JadedEncryption.OnewayEncryption();
+            if(onewayEncryption.VerifyHash(model.Password, "password"))
+            {
+                //Save login to session
+                HttpContext.Session.SetString("username", model.Username);
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else 
+            {
+                ModelState.AddModelError("Password", "Invalid username or password");
+            }
+        }
+        return View();
     }
 }
